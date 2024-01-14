d81b4702597be9711e72ddbe0e12910b1985e2b4
Ghassene Jebali <jbali.ghassen@gmail.com>
2024-01-14 18:06:40 +0100

    Profiler decorator

---
+++ python_memory_profiler/tools.py
@@ -0,0 +1,24 @@
+import psutil
+import sys
+import os
+import time
+
+from .profiler import Profiler
+
+if not (psutil.LINUX or psutil.MACOS or psutil.WINDOWS):
+    sys.exit("platform not supported")
+
+
+def profile_memory_decorator(function):
+    def wrapper():
+        pid = os.getpid()
+        profiler_instance = Profiler(pid)
+        profiler_instance.start()
+        # Give some time to the profiler to initialize
+        time.sleep(0.1)
+        function()
+        time.sleep(0.1)
+        profiler_instance.save()
+        profiler_instance.plot("data")
+
+    return wrapper
